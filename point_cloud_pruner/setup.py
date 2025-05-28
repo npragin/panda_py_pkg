9221e99eeda43e3626a4645c035edc3edf6553b5
npragin <npragin@gmail.com>
2025-05-28 15:50:29 -0700

    Add entry point for camera calibrator.

--- point_cloud_pruner/setup.py
+++ point_cloud_pruner/setup.py
@@ -37,6 +37,7 @@ setup(
         'console_scripts': [
             # A node that prunes the point cloud
             'prune_pointcloud = point_cloud_pruner.point_cloud_pruner:main',
-       ],
+            'calibrate_camera = point_cloud_pruner.camera_calibrator:main',
+        ],
     },
 )
