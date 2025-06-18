9a4300402c0244989d68d76410fe011b9d181f08
npragin <npragin@gmail.com>
2025-06-18 14:07:24 -0700

    Create perception.py launch file.

---
+++ panda_manager/launch/perception.py
@@ -0,0 +1,28 @@
+# Launch file for the perception system on the Panda robot.
+#
+# Noah Pragin
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription(
+        [
+            Node(
+                package="point_cloud_pruner",
+                executable="calibrate_camera",
+                name="camera_calibrator",
+            ),
+            Node(
+                package="point_cloud_pruner",
+                executable="point_cloud_transformer",
+                name="point_cloud_transformer",
+            ),
+            Node(
+                package="point_cloud_pruner",
+                executable="prune_pointcloud",
+                name="pruner",
+            ),
+        ]
+    )
